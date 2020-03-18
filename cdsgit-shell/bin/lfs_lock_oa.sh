2827fbd5dd5d4b65acc7de8b2681f28cbc77b584
Cadence <cadence@cadence-server.cryptoquantique.com>
2020-03-18 10:54:10 +0000

    checkout latest version of files before locking them

--- cdsgit-shell/bin/lfs_lock_oa.sh
+++ cdsgit-shell/bin/lfs_lock_oa.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 #git lsf unlock all users' locked *.oa files underneath given directory
 
-LVCdir=$(realpath $1)
+LCVdir=$(realpath $1)
 #rootdir=$(realpath $2)
 output=()
-cd ${LVCdir}
+cd ${LCVdir}
 files=$(find . -name '*.oa')
 
 for f in $files
 	do
-		lockCount=0
 		if [ -z "$(git lfs locks --path=${f})" ]
 		then
-			git lfs lock $f
+			#checkout latest before we lock the file
+			git lfs lock ${f} && git fetch && git checkout origin/master -- $(dirname ${f})
 		else
 			echo "${f} already locked"
 		fi
