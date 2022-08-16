23da44be8eccfddd1848f7220abb34824501584d
Cadence <cadence@cryptoquantique.com>
2022-08-16 11:13:56 +0100

    Detect v3+ of LFS and add ability to pass multiple files to lock operations. Add force option to Unlock.

--- cdsgit-shell/bin/lfs_lock_oa.sh
+++ cdsgit-shell/bin/lfs_lock_oa.sh
@@ -1,20 +1,35 @@
 #!/bin/bash
-#git lsf lock *.oa and *.sdb files underneath given directory
 
-LCVdir=$(realpath $1)
-#rootdir=$(realpath $2)
-output=()
+p=$(realpath $1)
+#if path is a file then return the parent directory
+if [ -f "$1" ]
+then
+	LCVdir=$(dirname ${p})
+else
+	LCVdir=${p}
+fi
+
 cd ${LCVdir}
-files=$(find . -type f \( -name '*.oa' -o -name '*.sdb' \))
-
-for f in $files
+# get locakable files for this repo
+files=$(/bin/bash $CDSGIT_PATH/cdsgit-shell/bin/find_lockable.sh "$LCVdir")
+#echo "Files: ${files}"
+if git lfs --version | grep -q "^git-lfs/3\.[0-9]\.[0-9]"
+then
+	# With git-lfs v3.0.0 and higher we can lock multiple files as a batch
+	#echo "git-lfs v3 or higher detected"
+	dirs=$(dirname ${files})
+	git lfs lock ${files} && git fetch && git checkout origin/master -- $dirs
+else
+	for f in $files
 	do
 		if [ -z "$(git lfs locks --path=${f})" ]
 		then
 			#checkout latest before we lock the file
 			git lfs lock ${f} && git fetch && git checkout origin/master -- $(dirname ${f})
+			echo "Locking file ${f}"
 		else
 			echo "${f} already locked"
 		fi
 	done
+fi
 
--- cdsgit-shell/bin/lfs_unlock_oa.sh
+++ cdsgit-shell/bin/lfs_unlock_oa.sh
@@ -1,40 +1,51 @@
 #!/bin/bash
-#git lsf unlock all users' locked *.oa  and *.sdb files underneath given directory
 
-path=$(realpath $1)
-
-#if path is a file then return the parent directory
-if [ -f "$1" ]
-then
-	LCVdir=$(dirname ${path})
-else
-	LCVdir=${path}
-fi
-
-cd ${LCVdir}
 #get repo root
 rootstr=$(git rev-parse --show-toplevel)
-
-#find all the oa and sdb files under the given directory
-files=$(find ${path} -type f \( -name '*.oa' -o -name '*.sdb' \))
-#get paths of user's own locks for current repo
-locks=$(git lfs locks --verify | grep "^O" | awk '{ print $2 }')
-
 cd ${rootstr}
-for lf in $locks
-   do
-      #check if this locked file is one in our list of potential files to unlock
-      if [[ "$files" == *"$lf"* ]]
-      then
-         if [ -n "$(git diff HEAD ${lf})" ] 
-         then
-            echo "Can't unlock ${lf} : file is locally modified"
-         else
-            #not locally modified so we can unlock it
-            #echo "${lf} will be unlocked!";
-            git lfs unlock ${lf}
-         fi
-      fi
-   done
+if [ "$1" = "-f" ]
+then
+	force="TRUE"
+	shift
+fi
 
+locks="$@"
+
+files_to_unlock=()
+
+for lf in $locks
+do
+   diffresult=$(git diff HEAD -- ${lf})
+   if [[ -n "${diffresult}" ]] 
+   then
+      echo "Can't unlock ${lf} : file is locally modified"
+   else
+      files_to_unlock+="$lf "
+   fi   
+done
+
+if [[ ! -z "$files_to_unlock" ]]
+then
+   #if using newer git-lfs we can unlock files as a batch
+   if git lfs --version | grep -q "^git-lfs/3\.[0-9]\.[0-9]"
+   then
+    	if [[ "${force}" = "TRUE" ]]
+      	then
+			git lfs unlock --force ${files_to_unlock}
+		else
+			git lfs unlock ${files_to_unlock}
+		fi
+   else
+   #for older git-lfs we need to unlock each file individually
+      for f in $files_to_unlock
+      do
+        if [[ "${force}" = "TRUE" ]]
+      	then
+        	git lfs unlock --force ${f}
+        else
+        	git lfs unlock ${f}
+        fi
+      done
+   fi
+fi
 
