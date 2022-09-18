23a65895803268c6a502053353fc7df462a33e0e
Cadence <cadence@cryptoquantique.com>
2022-09-18 16:05:31 +0100

    various bug fixes and improvements to locking functions

--- cdsgit-shell/bin/lfs_lock_oa.sh
+++ cdsgit-shell/bin/lfs_lock_oa.sh
@@ -26,23 +26,32 @@ done
 files=$(find $LCVdir -type f \( "${find_cmd[@]}" \))
 
 #echo "Files: ${files}"
-if git lfs --version | grep -q "^git-lfs/3\.[0-9]\.[0-9]"
+if [ -z "$files" ]
 then
-	# With git-lfs v3.0.0 and higher we can lock multiple files as a batch
-	#echo "git-lfs v3 or higher detected"
-	dirs=$(dirname ${files})
-	git lfs lock ${files} && git fetch && git checkout origin/master -- $dirs
+    echo "Nothing to lock in path $LCVdir"
 else
+    # If dir contains tracked files then checkout latest before we lock
+    if [ ! -z "$(git ls-files $LCVdir)" ]
+    then
+        echo "Checking out latest version from remote"
+        git fetch && git checkout origin/master -- $LCVdir
+    fi
+    if git lfs --version | grep -q "^git-lfs/3\.[0-9]\.[0-9]"
+    then
+	# With git-lfs v3.0.0 and higher we can lock multiple files as a batch
+	git lfs lock $files
+    else
 	for f in $files
 	do
-		if [ -z "$(git lfs locks --path=${f})" ]
-		then
-			#checkout latest before we lock the file
-			git lfs lock ${f} && git fetch && git checkout origin/master -- $(dirname ${f})
-			echo "Locking file ${f}"
-		else
-			echo "${f} already locked"
-		fi
+	  #  if [ -z "$(git lfs locks --path=${f})" ]
+	  #  then
+		#checkout latest before we lock the file
+            git lfs lock $f
+#		echo "Locking file ${f}"
+#	    else
+#		echo "${f} already locked"
+#	    fi
 	done
+    fi
 fi
 
